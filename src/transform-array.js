31afdd0ddbf7f9c56bb1e744289a6f9c358ec610
Danil Chemodanov <dan.cmdn@gmail.com>
2020-11-01 20:34:22 +0300

    feat: solve all tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,39 +1,37 @@
 module.exports = function transform(arr) {
 	if (!Array.isArray(arr) || !arr) {
 		throw new Error();
-	};
+	}
 
-	let finArr = arr.slice();
-	for (let i = 0; i < finArr.length; i++) {
-		if (finArr[i] === '--discard-next') {
-			if (finArr[i + 2] === '--double-prev') {
-				finArr.splice(i, 3);
-			} else if (finArr[i + 2] === '--discard-prev') {
-				finArr.splice(i, 3);
-			} else if (i === arr.length - 1) {
-				finArr.splice(i, 1);
-			} else {
-				finArr.splice(i, 2);
-			}
-		} else if (finArr[i] === '--discard-prev') {
-			if (i === 0) {
-				finArr.splice(0, 1);
-			} else {
-				finArr.splice(i - 1, 2);
-			}
-		} else if (finArr[i] === '--double-next') {
-			if (i === arr.length - 1) {
-				finArr.splice(i, 1);
-			} else {
-				finArr[i] = finArr[i + 1];
-			}
-		} else if (finArr[i] === '--double-prev') {
-			if (i === 0) {
-				finArr.splice(0, 1);
-			} else {
-			finArr[i] = finArr[i - 1];
-			}
-		} 
-	};
-	return finArr;
-};
+	let finalArr = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		switch (arr[i]) {
+			case '--discard-prev':
+				finalArr.pop();
+				break;
+			case '--double-next':
+				if (arr[i + 1] !== undefined) {
+					finalArr.push(arr[i + 1]);
+				}
+				break;
+			case '--double-prev':
+				if (arr[i - 1] !== undefined) {
+					finalArr.push(arr[i - 1]);
+				}
+				break;
+			case '--discard-next':
+				if (arr[i + 2] && arr[i + 2].toString().includes('-prev')) {
+					i += 2;
+				} else {
+					i += 1;
+				}
+				break;
+			default:
+				finalArr.push(arr[i]);
+				break;
+		}
+	}
+
+	return finalArr;
+}
